f5f12679bec36024ff210a66c942f7be1cb32331
buildwith-krishna <krishnapandey7189@gmail.com>
2026-04-14 14:41:53 +0530

    v3: added update feature + improved notes system

--- notes.py
+++ notes.py
@@ -1,39 +1,137 @@
+FILE_NAME = "notes.txt"
+            
+def add_note():
+
+    note = input("Enter your note: ").strip()
+    
+    if note == "":
+        print("Notes cannot be empty!" + "\n")
+    else:
+        with open(FILE_NAME, "a") as file:
+            file.write(note.strip() + "\n")
+            print("<--Note added successfully-->" + "\n")
+
+
+def get_notes():
+
+    try:
+        with open(FILE_NAME, "r") as file:
+            notes = file.read().split("\n")
+
+            clean_notes = []
+            for note in notes:
+                if note != "":
+                    clean_notes.append(note)
+                
+            return clean_notes
+
+    except FileNotFoundError:
+        return []
+
+    except Exception as E:
+        print(f"Unexpected error: {E}")
+        return []
+
+        
+def display_notes(clean_notes):
+                
+    for i, note in enumerate(clean_notes, start=1):
+
+        if note == "":
+            print("^Notes are empty!^")
+        else:
+            print(f"{i}. {note}")
+    
+def del_note(clean_notes):
+
+    display_notes(clean_notes)
+    print("\n")
+            
+    try:         
+        index = int(input("Select index: ").strip())
+        print("\n")
+    except ValueError:
+        print("invaid input! Enter numbers only.")
+
+    if index <= len(clean_notes) and index >= 1:                
+        modified_notes = []
+        for i, note in enumerate(clean_notes, start=1):
+            if i != index:
+                modified_notes.append(note)
+
+        with open(FILE_NAME, "w") as file:
+            file.write("\n".join(modified_notes) + "\n")    
+        print("<<--Note deleted successfully-->>" + "\n")
+
+    else:
+        print("That index doesn't exist!")
+
+def update_note(clean_notes):
+
+    print("<<-Choose number to update->>")
+    display_notes(clean_notes)
+    print("\n")
+
+    try:
+        number = int(input("Enter number: ").strip())
+    except ValueError:
+        print("Enter number of the note only!")
+    updated_notes = []
+    
+    for i, note in enumerate(clean_notes, start=1):
+        if i != number:
+            updated_notes.append(note)
+            
+        else:
+            new_note = input("Enter new note: ").strip()                
+            updated_notes.append(new_note)
+            
+    with open(FILE_NAME, "w") as file:
+        file.write("\n".join(updated_notes) + "\n")
+
+    print("<<--Note updated successfully-->>")
+    print("\n")        
+            
+    
 def main():
 
+    notes = get_notes()
     print("<---WELCOME TO NOTES APP--->" + "\n")
-    
+
     while True:
-    
-        print("<---Choose your option--->" + "\n")
+
         print("1. Add a note")
-        print("2. view notes")
-        print("3. exit")
+        print("2. View notes")
+        print("3. Delete a note")
+        print("4. Update a note")
+        print("5. Exit")
 
         choice = input("Enter your choice: ").strip()
-
+        print("\n")
+                
         if choice == "1":
-
-            note = input("Enter your note: ")
-
-            with open("notes2.txt", "a") as file:
-                file.write(note + "\n")
-
-            print("<--Note added successfully-->") 
+            add_note()
+            notes = get_notes()
 
         elif choice == "2":
-            try:
-                with open("notes2.txt", "r") as file:
-                    print(file.read())
-                    
-            except FileNotFoundError:
-                print("Note is empty! Please add a note first.")
-
-        elif choice == "3":
-            print("Goodbye")
-            break        
-
-        else: 
-            print("Invalid input! Please enter choice from 1 to 3.")
-
+            display_notes(notes)
+            print("\n")
             
+        elif choice == "3":
+            del_note(notes)
+            notes = get_notes()
+            print("\n")
+
+        elif choice == "4":
+            update_note(notes)
+            notes = get_notes()
+            print("\n") 
+
+        elif choice == "5":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid input! Enter choice from 1 to 5." + "\n")
+
 main()
